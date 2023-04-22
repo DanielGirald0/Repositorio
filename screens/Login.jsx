391c0959ebfd265e58f40f8bd2f417b4dbb01d82
Daniel Giraldo Hurtado <dghurtado.13@gmail.com>
2023-04-21 22:10:00 -0500

    creo el login

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,11 +1,18 @@
-import { View,Text,Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import { styles } from "../assets/styles/styles";
-import { TextInput } from "react-native-web";
 
-export default function Login(){
-    return(
-        <View>
-            
-        </View>
-    )
+export default function Login({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontWeight: "bold", marginBottom: 10 }}>
+        Estamos en Login
+      </Text>
+      <Button
+        title="Iniciar sesión"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
 }
